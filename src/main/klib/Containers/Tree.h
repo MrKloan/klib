f962a409741170dba4aa16b1b835716dedf3de24
Valentin Fries <contact@fries.io>
2015-09-17 11:40:34 +0200

    Tree base header

--- src/main/klib/Containers/Tree.h
+++ src/main/klib/Containers/Tree.h
@@ -1,12 +1,29 @@
 #ifndef KLIB_TREE_H
 #define	KLIB_TREE_H
 
+#include "Array.h"
+
 #ifdef	__cplusplus
 extern "C" {
 #endif
 
+typedef struct Branch {
+	void *leave;
+	
+	Array *nodes;
+	size_t size;
+} Branch;
 
+typedef struct Tree {
+	Branch *trunk;
+} Tree;
 
+void libexp TreeNew(void);
+void libexp TreeInit(Tree*);
+void libexp TreeFree(Tree*);
+void libexp TreeClear(Tree*);
+
+Branch* libexp TreeGetNode(Branch*, size_t);
 
 #ifdef	__cplusplus
 }
